5a2da8642d6c0be1933373131cc1895e68d7a575
Ubuntu <ubuntu@ip-172-26-0-45.ec2.internal>
2021-07-29 18:15:59 +0000

    new

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AlmaginetxPlay.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inkacoin.settings")
 
     from django.core.management import execute_from_command_line
 
